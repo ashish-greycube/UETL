bfbefa0bf1971633a0e4bedfad1d93e5623ee498
vr-greycube <vijay@greycube.in>
2023-09-25 13:16:49 +0530

    feat: add api for filter values for sales tracker direct report

--- uetl/api.py
+++ uetl/api.py
@@ -1,6 +1,7 @@
 import frappe
 from frappe.utils.csvutils import build_csv_response
 from uetl.uetl.report.sales_tracker_direct.sales_tracker_direct import execute
+from frappe.desk.search import search_link
 
 
 @frappe.whitelist(allow_guest=False)
@@ -18,3 +19,38 @@ def get_sales_tracker(**filters):
     for d in data:
         csv.append([d.get(f) for f in fieldnames])
     build_csv_response(csv, "sales_tracker")
+
+
+@frappe.whitelist(allow_guest=False)
+def get_sales_tracker_filter(field_name):
+    """
+    Sales Person, Cost Center: name , parent
+    Customer: name, customer_name, customer_group, territory, mobile_no, primary_address,
+    """
+    frappe.set_user("Administrator")
+    link_fields = [
+        "sales_person",
+        "cost_center",
+        "customer",
+        "brand",
+        "territory",
+    ]
+    if field_name in (
+        "on_order_np_qty",
+        "reserved_order_qty",
+        "reserved_physical_qty",
+        "sold_qty",
+    ):
+        return ["> 0", "= 0"]
+    elif field_name in ("so_status",):
+        return ["Open", "Closed", "All"]
+    elif field_name in ("mr_date",):
+        return ["Yesterday", "Today"]
+    elif field_name in link_fields:
+        search_link(
+            frappe.unscrub(field_name),
+            txt="",
+            ignore_user_permissions=True,
+            page_length=1000,
+        )
+        return frappe.response.get("results")
